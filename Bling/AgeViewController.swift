e29bc2cd4d1e2cbae6f86a5ce3b00ddb3f9da78b
Quynh Tran <nhim0406@Quynhs-MacBook-Pro.local>
2016-02-27 10:51:43 +0000

    added view controller swift files

---
+++ Bling/AgeViewController.swift
@@ -0,0 +1,45 @@
+//  Created by Quynh Tran and Saci Sebo on 27/02/2016.
+//  Copyright © 2016 Quynh. All rights reserved.
+//
+
+import UIKit
+
+class AgeViewController: UIViewController {
+    
+    //Link UI elements here
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    } //End of viewDidLoad
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        //Initial settings of UI elements
+        
+        
+        
+        
+        
+        
+        
+    } //End of viewWillAppear
+    
+    override func viewDidAppear(animated: Bool) {
+        //animations
+        
+        
+        
+        
+    }//End of viewDidAppear
+    
+    
+    
+    
+} //End of class
